935ca3a7290daf6d4973c76fc2899578cd0b846d
Leonardo Padilha <leonardopadilha28@gmail.com>
2026-08-15 14:47:00 -0300

    Iniciando curso - Método ADT AI

---
+++ tests/app.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+
+test('Deve exibir o slogan na aba do navegador', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Lunar Pass/)
+})
